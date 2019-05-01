28ec904e64d2f952ee370cfae6ec91ecf3c6ce1e
Nguyen Dai <taidai1569@gmail.com>
2019-05-01 22:18:26 +0700

    Done feature in result

--- protocal/govasResult.go
+++ protocal/govasResult.go
@@ -1,1 +1,47 @@
 package protocal
+
+import (
+	"errors"
+	"github.com/beevik/etree"
+	"github.com/sirupsen/logrus"
+)
+
+// get all result
+func (gomd *Govasmd) GetResults(
+	filter string,
+	filterId string,
+	taskId string,
+	noteDetails int,
+	overrideDetails int,
+	details int) (string, error) {
+
+	doc := etree.NewDocument()
+	cmd := doc.CreateElement("get_results")
+	addFilter(cmd, filter, filterId)
+	if taskId != "" {
+		cmd.CreateAttr("task_id", taskId)
+	}
+	if checkIntDefine(details) {
+		cmd.CreateAttr("note_details", string(details))
+	}
+	if checkIntDefine(noteDetails) {
+		cmd.CreateAttr("note_details", string(noteDetails))
+	}
+	if checkIntDefine(overrideDetails) {
+		cmd.CreateAttr("override_details", string(overrideDetails))
+	}
+	return gomd.sendXmlCommand(doc)
+}
+
+func (gomd *Govasmd) GetResult(resultId string) (string, error) {
+	if resultId == "" {
+		err := errors.New("create_target requires a resultId argument")
+		logrus.Error("In create_target()", err)
+		return "", err
+	}
+	doc := etree.NewDocument()
+	cmd := doc.CreateElement("get_results")
+	cmd.CreateAttr("result_id", resultId)
+	cmd.CreateAttr("details", "1")
+	return gomd.sendXmlCommand(doc)
+}
--- utils/result.go
+++ utils/result.go
@@ -1,1 +1,55 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type payloadGetResults struct {
+	Filter          string `form:"filter"`
+	FilterId        string `form:"filter_id"`
+	TaskId          string `form:"task_id"`
+	NoteDetails     int    `form:"note_details"`
+	OverrideDetails int    `form:"override_details"`
+	Details         int    `form:"details"`
+}
+
+// get all results
+func GetResults(c *gin.Context) {
+	logrus.Info("\n\n-------Get all results in scanner--------")
+	var payload payloadGetResults
+	payload.NoteDetails = -1
+	payload.OverrideDetails = -1
+	payload.Details = -1
+	_ = c.ShouldBindQuery(&payload)
+	fmt.Println(payload)
+	govas := AuthConnection(c)
+	respond, err := govas.GetResults(payload.Filter, payload.FilterId, payload.TaskId, payload.NoteDetails, payload.OverrideDetails, payload.Details)
+	if err != nil {
+		logrus.Error("Get result fail: ", err)
+		respond = CreatJsonErro(err.Error())
+		c.Data(http.StatusBadRequest, "application/json; charset=utf-8", []byte(respond))
+	} else {
+		logrus.Info("Get results success")
+		c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(respond))
+	}
+	_ = govas.Disconnect()
+}
+
+func GetResult(c *gin.Context) {
+	logrus.Info("\n\n-------Get result in scanner--------")
+	resultId := c.Param("result_id")
+	govas := AuthConnection(c)
+	respond, err := govas.GetResult(resultId)
+	if err != nil {
+		logrus.Error("Get result fail: ", err)
+		respond = CreatJsonErro(err.Error())
+		c.Data(http.StatusBadRequest, "application/json; charset=utf-8", []byte(respond))
+	} else {
+		logrus.Info("Get result success")
+		c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(respond))
+	}
+	_ = govas.Disconnect()
+}
