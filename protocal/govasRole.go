75f3e0db8a2bbd959ded90ce78141c4d07dd679e
Nguyen Tai Dai <taidai1569@gmail.com>
2019-05-03 13:00:53 +0700

    update feature-restore

--- protocal/govasRole.go
+++ protocal/govasRole.go
@@ -1,6 +1,11 @@
 package protocal
 
-import "github.com/beevik/etree"
+import (
+	"errors"
+	"github.com/beevik/etree"
+	"github.com/sirupsen/logrus"
+	"strings"
+)
 
 // Get Roles
 func (gomd *Govasmd) GetRoles(filter string, filter_id string) (string, error) {
@@ -18,6 +23,65 @@ func (gomd *Govasmd) GetRole(uuid string) (string, error) {
 	return gomd.sendXmlCommand(doc)
 }
 
-//func (gomd *Govasmd) DeleteRole(uuid string) (string, error) {
-//
-//}
+func (gomd *Govasmd) DeleteRole(roleID string, ultimate bool) (string, error) {
+	if roleID == "" {
+		err := errors.New("DeleteRole requires a roleID argument")
+		logrus.Error("In DeleteRole() ", err)
+		return "", err
+	}
+	doc := etree.NewDocument()
+	cmd := doc.CreateElement("delete_role")
+	cmd.CreateAttr("role_id", roleID)
+	cmd.CreateAttr("ultimate", toString(ultimate))
+	return gomd.sendXmlCommand(doc)
+}
+
+func (gomd *Govasmd) CloneRole(roleId string) (string, error) {
+	if roleId == "" {
+		err := errors.New("CloneRole requires a roleId argument")
+		logrus.Error("In CloneRole() ", err)
+		return "", err
+	}
+	doc := etree.NewDocument()
+	cmd := doc.CreateElement("create_role")
+	cmd.CreateElement("copy").CreateCharData(roleId)
+	return gomd.sendXmlCommand(doc)
+}
+
+func (gomd *Govasmd) CreateRole(name string, comment string, users []string) (string, error) {
+	if name == "" {
+		err := errors.New("CreateRole Requires name")
+		logrus.Error("In CreateRole()", err)
+		return "", err
+	}
+	doc := etree.NewDocument()
+	cmd := doc.CreateElement("create_role")
+	cmd.CreateElement("name").CreateCharData(name)
+	if comment != "" {
+		cmd.CreateElement("comment").CreateCharData(comment)
+	}
+	if len(users) > 0 {
+		cmd.CreateElement("users").CreateCharData(strings.Join(users, ","))
+	}
+	return gomd.sendXmlCommand(doc)
+}
+func (gomd *Govasmd) UpdateRole(roleId, name, comment string, users []string) (string, error) {
+	if name == "" {
+		err := errors.New("UpdateRole Requires roleId")
+		logrus.Error("In UpdateRole()", err)
+		return "", err
+	}
+	doc := etree.NewDocument()
+	cmd := doc.CreateElement("modify_role")
+	cmd.CreateAttr("role_id", roleId)
+	if name != "" {
+		cmd.CreateElement("name").CreateCharData(name)
+	}
+	if comment != "" {
+		cmd.CreateElement("comment").CreateCharData(comment)
+	}
+	if len(users) > 0 {
+		cmd.CreateElement("users").CreateCharData(strings.Join(users, ","))
+	}
+	return gomd.sendXmlCommand(doc)
+}
